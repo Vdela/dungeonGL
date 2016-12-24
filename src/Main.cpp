3aa06b401ad4a05ebd859b6e75e02d2f3e185cf4
iReine <irene.blt@free.fr>
2016-12-24 16:27:22 +0100

    Irene : implementation niveau et lecture fichier ATTENTION erreurs

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,13 +2,16 @@
 
 
 #include "../include/GameManager.h"
-
+#include "../include/Niveau.h"
 
 
 int main(int argc, char **argv) {
 
     GameManager gameManager = GameManager::getInstance();
     gameManager.initGame( 800, 800, (char*)"DungeonGL" );
+
+    Niveau niveau1;
+    niveau1.lectureMap((char*)"../files/map3.ppm");
 
     return 0;
 }
